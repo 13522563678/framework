12838a7ebdf815825eb6cefba4297c83173a3560
yaoyf <yaoyf@kcwl.com>
2022-08-18 16:50:27 +0800

    新增参数校验组件

---
+++ kcwl-ddd-core/src/main/java/com/kcwl/ddd/infrastructure/utils/validator/group/Group.java
@@ -0,0 +1,13 @@
+package com.kcwl.ddd.infrastructure.utils.validator.group;
+
+import javax.validation.GroupSequence;
+
+/**
+ * 定义校验顺序，如果AddGroup组失败，则UpdateGroup组不会再校验
+ *
+ * @author yaoyf
+ */
+@GroupSequence({AddGroup.class, UpdateGroup.class})
+public interface Group {
+
+}
---
+++ kcwl-ddd-core/src/main/java/com/kcwl/ddd/infrastructure/utils/validator/group/UpdateGroup.java
@@ -0,0 +1,11 @@
+package com.kcwl.ddd.infrastructure.utils.validator.group;
+
+/**
+ * 更新数据 Group
+ *
+ * @author yaoyf
+ */
+
+public interface UpdateGroup {
+
+}

8e7ebd791b87cb8f1cb4cdab2edb4414078dd14e
renyp <renyp@kcwl.com>
2023-02-28 21:33:30 +0800

    feat: 错误码提示语 热更新

---
+++ kcwl-common-rest/src/test/java/com/kcwl/framework/rest/helper/ResponseHelperTest.java
@@ -0,0 +1,62 @@
+package com.kcwl.framework.rest.helper;
+
+import cn.hutool.extra.spring.EnableSpringUtil;
+import com.kcwl.ddd.domain.entity.UserAgent;
+import com.kcwl.ddd.infrastructure.api.IErrorPromptDecorator;
+import com.kcwl.ddd.infrastructure.api.ResponseMessage;
+import com.kcwl.ddd.infrastructure.session.SessionContext;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * <p>
+ * 错误码 热更新 单测
+ * </p>
+ *
+ * @author renyp
+ * @since 2023/2/28 16:46
+ */
+
+@RunWith(SpringRunner.class)
+@EnableSpringUtil
+@SpringBootTest(classes = ResponseHelperTest.class)
+class ResponseHelperTest {
+
+    private final String expectedMessage = "这是 被替换的提示信息！";
+    private final String defaultMessage = "这是 默认的提示信息！";
+
+    @MockBean
+    private IErrorPromptDecorator errorPromptDecorator;
+
+    @BeforeEach
+    public void setUp() {
+        Mockito.reset(errorPromptDecorator);
+        Mockito.when(errorPromptDecorator.getErrorPrompt("404", "1")).thenReturn(expectedMessage);
+        Mockito.when(errorPromptDecorator.getErrorPrompt("403", "1")).thenReturn(defaultMessage);
+
+        UserAgent userAgent = new UserAgent();
+        userAgent.setProduct("1");
+        SessionContext.setRequestUserAgent(userAgent);
+    }
+
+
+    @Test
+    void createFailMessage() {
+
+        ResponseMessage<?> failMessage = ResponseHelper.createFailMessage("404", defaultMessage);
+        Assert.assertEquals(expectedMessage, failMessage.getMessage());
+
+        ResponseMessage<?> failMessage1 = ResponseHelper.createFailMessage("403", defaultMessage);
+        Assert.assertEquals(defaultMessage, failMessage1.getMessage());
+
+        ResponseMessage<?> failMessage2 = ResponseHelper.createFailMessage("403", defaultMessage);
+        Assert.assertEquals(defaultMessage, failMessage2.getMessage());
+
+    }
+}
--- kcwl-ddd-core/src/main/java/com/kcwl/ddd/infrastructure/api/IErrorPromptDecorator.java
+++ kcwl-ddd-core/src/main/java/com/kcwl/ddd/infrastructure/api/IErrorPromptDecorator.java
@@ -6,9 +6,9 @@ package com.kcwl.ddd.infrastructure.api;
 public interface IErrorPromptDecorator {
     /**
      * 获取错误提示语
-     * @param code 错误码
+     * @param code    错误码
      * @param product 端类型
-     * @return 返回错误提示语,如果没有定义错误提示语，返回null或空字符串；
+     * @return 返回错误提示语, 如果没有定义错误提示语，返回null或空字符串；
      */
     String getErrorPrompt(String code, String product);
 }
